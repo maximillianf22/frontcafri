81bb6dd645a5ed23fd87cfe83c0010e76aa28df3
maximillianf22 <dgse.informatica@gmail.com>
2020-04-18 14:28:38 -0500

    actualizaciones

--- resources/views/theme/layouts/store/listCategoryProducts.blade.php
+++ resources/views/theme/layouts/store/listCategoryProducts.blade.php
@@ -1,7 +1,16 @@
 @extends('theme.templates.'.$Config_->nameDirectory.'.'.$Config_->themeTemplate)
 @section('content-store-theme')
 @include('theme.includes.defines')
-<div style="height:80px;"></div>
+<div class="header-filter contactus-3 bg-primary" style="background-image: url('{{asset('front/images/header.jpg')}}'); background-attachment: fixed background-repeat:   no-repeat;  background-position: top; height: 35vh;">
+      <div class="page-header-image"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+              
+          </div>
+        </div>
+      </div>
+    </div>
 <div class="container-fluid">
      <div class="row pad-all" >
           @include(JPATH_COMPONENTS . 'store.comp_offers.show')
