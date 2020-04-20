89042addad12a377c7bf2f3b839e73b2e9fc030c
maximillianf22 <dgse.informatica@gmail.com>
2020-04-20 15:49:55 -0500

    carousel de productos

--- resources/views/theme/layouts/store/index.blade.php
+++ resources/views/theme/layouts/store/index.blade.php
@@ -5,7 +5,6 @@
 @include(JPATH_COMPONENTS . 'core.comp_slider.index')
 <div class="container-fluid">
      <div class="row ">
-         @include(JPATH_COMPONENTS . 'store.comp_offers.show')
          @include(JPATH_COMPONENTS . 'store.comp_categories.show')
      </div>
 </div>
