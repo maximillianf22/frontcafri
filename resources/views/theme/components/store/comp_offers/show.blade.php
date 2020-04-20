cb2f98cdee80bc03001e2e6ea34b205bf3ef6f24
maximillianf22 <dgse.informatica@gmail.com>
2020-04-20 14:53:40 -0500

    carousel de productos

--- resources/views/theme/components/store/comp_offers/show.blade.php
+++ resources/views/theme/components/store/comp_offers/show.blade.php
@@ -189,13 +189,18 @@
 @endif 
 
 <script>
+     /*
+    Carousel
+*/
 $('#carouselExample').on('slide.bs.carousel', function (e) {
-
+    /*
+        CC 2.0 License Iatek LLC 2018 - Attribution required
+    */
     var $e = $(e.relatedTarget);
     var idx = $e.index();
-    var itemsPerSlide = 3;
+    var itemsPerSlide = 5;
     var totalItems = $('.carousel-item').length;
-    
+ 
     if (idx >= totalItems-(itemsPerSlide-1)) {
         var it = itemsPerSlide - (totalItems - idx);
         for (var i=0; i<it; i++) {
