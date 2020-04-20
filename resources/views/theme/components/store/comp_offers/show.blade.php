35e21e1740ef4da10ea27b86ffcf6fb623662846
maximillianf22 <dgse.informatica@gmail.com>
2020-04-20 14:48:33 -0500

    carousel de productos

--- resources/views/theme/components/store/comp_offers/show.blade.php
+++ resources/views/theme/components/store/comp_offers/show.blade.php
@@ -1,4 +1,72 @@
+<style type="text/css">
+     @media (min-width: 768px) {
 
+    /* show 3 items */
+    .carousel-inner .active,
+    .carousel-inner .active + .carousel-item,
+    .carousel-inner .active + .carousel-item + .carousel-item,
+    .carousel-inner .active + .carousel-item + .carousel-item + .carousel-item  {
+        display: block;
+    }
+    
+    .carousel-inner .carousel-item.active:not(.carousel-item-right):not(.carousel-item-left),
+    .carousel-inner .carousel-item.active:not(.carousel-item-right):not(.carousel-item-left) + .carousel-item,
+    .carousel-inner .carousel-item.active:not(.carousel-item-right):not(.carousel-item-left) + .carousel-item + .carousel-item,
+    .carousel-inner .carousel-item.active:not(.carousel-item-right):not(.carousel-item-left) + .carousel-item + .carousel-item + .carousel-item {
+        transition: none;
+    }
+    
+    .carousel-inner .carousel-item-next,
+    .carousel-inner .carousel-item-prev {
+      position: relative;
+      transform: translate3d(0, 0, 0);
+    }
+    
+    .carousel-inner .active.carousel-item + .carousel-item + .carousel-item + .carousel-item + .carousel-item {
+        position: absolute;
+        top: 0;
+        right: -25%;
+        z-index: -1;
+        display: block;
+        visibility: visible;
+    }
+    
+    /* left or forward direction */
+    .active.carousel-item-left + .carousel-item-next.carousel-item-left,
+    .carousel-item-next.carousel-item-left + .carousel-item,
+    .carousel-item-next.carousel-item-left + .carousel-item + .carousel-item,
+    .carousel-item-next.carousel-item-left + .carousel-item + .carousel-item + .carousel-item,
+    .carousel-item-next.carousel-item-left + .carousel-item + .carousel-item + .carousel-item + .carousel-item {
+        position: relative;
+        transform: translate3d(-100%, 0, 0);
+        visibility: visible;
+    }
+    
+    /* farthest right hidden item must be abso position for animations */
+    .carousel-inner .carousel-item-prev.carousel-item-right {
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        display: block;
+        visibility: visible;
+    }
+    
+    /* right or prev direction */
+    .active.carousel-item-right + .carousel-item-prev.carousel-item-right,
+    .carousel-item-prev.carousel-item-right + .carousel-item,
+    .carousel-item-prev.carousel-item-right + .carousel-item + .carousel-item,
+    .carousel-item-prev.carousel-item-right + .carousel-item + .carousel-item + .carousel-item,
+    .carousel-item-prev.carousel-item-right + .carousel-item + .carousel-item + .carousel-item + .carousel-item {
+        position: relative;
+        transform: translate3d(100%, 0, 0);
+        visibility: visible;
+        display: block;
+        visibility: visible;
+    }
+
+}
+</style>
 
 @if(sizeof($Offers_)>=1)
      <div class="mercado__title wrapper center txt-center">
@@ -13,13 +81,12 @@
           <div class="row pad-all">
 
 
-
-                       <div id="carousel-producto" class="carousel slide" data-ride="carousel">
-                           <div class="carousel-inner row w-100 mx-auto" role="listbox">
+    <div id="carouselExample" class="carousel slide" data-ride="carousel" data-interval="9000">
+        <div class="carousel-inner row w-100 mx-auto" role="listbox">
                @foreach( $Offers_ as $index => $item)
                @if($index == 0)
                <div id="product-{{$item->id}}" 
-                    class="col-6 col-md-2 col-sm-4 carousel-item col-12 col-sm-6 col-md-4 col-lg-3 active"  style="padding:5px 7px !important"
+                    class="carousel-item col-md-3 active"  style="padding:5px 7px !important"
                     onclick="viewProduct({{$item->id}})" >
                     <div class="sc-item-store ">
                          <div class="categorie"> 
@@ -55,7 +122,7 @@
                </div>
                 @else
                 <div id="product-{{$item->id}}" 
-                    class="col-6 col-md-2 col-sm-4 carousel-item col-12 col-sm-6 col-md-4 col-lg-3"  style="padding:5px 7px !important"
+                    class="carousel-item col-md-3"  style="padding:5px 7px !important"
                     onclick="viewProduct({{$item->id}})" >
                     <div class="sc-item-store ">
                          <div class="categorie"> 
@@ -92,15 +159,15 @@
                @endif
                @endforeach
                </div>
-                           <a class="carousel-control-prev" href="#carousel-producto" role="button" data-slide="prev">
-                               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                               <span class="sr-only">Previous</span>
-                           </a>
-                           <a class="carousel-control-next" href="#carousel-producto" role="button" data-slide="next">
-                               <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                               <span class="sr-only">Next</span>
-                           </a>
-                       </div>
+                 <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+                      <i class="fa fa-chevron-left fa-lg text-muted"></i>
+                      <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next text-faded" href="#carouselExample" role="button" data-slide="next">
+                      <i class="fa fa-chevron-right fa-lg text-muted"></i>
+                      <span class="sr-only">Next</span>
+                  </a>
+               </div>
           </div>
      </div>
      </div>
@@ -122,18 +189,13 @@
 @endif 
 
 <script>
-     /*
-    Carousel
-*/
-$('#carousel-producto').on('slide.bs.carousel', function (e) {
-    /*
-        CC 2.0 License Iatek LLC 2018 - Attribution required
-    */
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+
     var $e = $(e.relatedTarget);
     var idx = $e.index();
-    var itemsPerSlide = 5;
+    var itemsPerSlide = 7;
     var totalItems = $('.carousel-item').length;
- 
+    
     if (idx >= totalItems-(itemsPerSlide-1)) {
         var it = itemsPerSlide - (totalItems - idx);
         for (var i=0; i<it; i++) {
@@ -148,19 +210,3 @@ $('#carousel-producto').on('slide.bs.carousel', function (e) {
     }
 });
 </script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- Top content -->
