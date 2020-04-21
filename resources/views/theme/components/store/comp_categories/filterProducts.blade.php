f7b21290b7d7dd79d7e4f0fabbf8c93b6e48da63
maximillianf22 <dgse.informatica@gmail.com>
2020-04-21 15:32:52 -0500

    validacion del modal

--- resources/views/theme/components/store/comp_categories/filterProducts.blade.php
+++ resources/views/theme/components/store/comp_categories/filterProducts.blade.php
@@ -46,6 +46,87 @@
                @if(sizeof($subcategoriasslug_))
               <h3 class="display-3">Categorias Disponibles</h3>
               <p class="lead mt-1">¡Explora nuestras variedad de productos en nuestras secciones!</p>
+               <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+        skdnfskjdfn <br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+               
                @endif
             </div>
           </div>
